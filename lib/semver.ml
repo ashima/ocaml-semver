4bda9b1e2099d4431146cbbb8e43b7f25a32a627
Tikhon Jelvis <tikhon@berkeley.edu>
2012-05-29 21:29:01 -0700

    Rewrote the semver module.

--- lib/semver.ml
+++ lib/semver.ml
@@ -1,55 +1,52 @@
-open Scanf
+open List
 open Printf
+open Scanf
 
-module Semver = struct
+type semver = Semver of int * int * int
 
-  type semver = Semver of int option * int option * int option
-  type semver_part = Major | Minor | Patch
+type query = QueryPatch of int * int * int
+           | QueryMinor of int * int
+           | QueryMajor of int
 
-  exception Invalid_version
+let compare_version l r = match l, r with
+    Semver (l1, l2, l3), Semver (r1, r2, r3) ->
+      match compare l1 r1, compare l2 r2, compare l3 r3 with
+          0, 0, res -> res
+        | 0, res, _ -> res
+        | res, _, _ -> res
 
-  let cmp_option = fun a b -> match a, b with
-      None, None -> 0
-    | None, _ -> 1
-    | _, None -> -1
-    | Some a, Some b when a > b -> 1
-    | Some a, Some b when b > a -> -1
-    | _ -> 0
+let increment_version p v = match p, v with
+    `Major, Semver (l1, _, _) -> Semver (l1 + 1, 0, 0)
+  | `Minor, Semver (l1, l2, _) -> Semver (l1, l2 + 1, 0)
+  | `Patch, Semver (l1, l2, l3) -> Semver (l1, l2, l3 + 1)
 
-  let compare_version = fun l r -> match l, r with
-      Semver (l1, l2, l3), Semver (r1, r2, r3) ->
-        match cmp_option l1 r1, cmp_option l2 r2, cmp_option l3 r3 with
-            0, 0, res -> res
-          | 0, res, _ -> res
-          | res, _, _ -> res
+let decrement_version p v = match p, v with
+    `Major, Semver (l1, _, _) -> Semver (l1 - 1, 0, 0)
+  | `Minor, Semver (l1, l2, _) -> Semver (l1, l2 - 1, 0)
+  | `Patch, Semver (l1, l2, l3) -> Semver (l1, l2, l3 - 1)
 
-  let inc_option = function None -> None | Some n -> Some (n + 1)
+let parse_version input = sscanf input "%d.%d.%d" (fun v1 v2 v3 -> Semver (v1, v2, v3))
 
-  let increment_version = fun p v -> match p, v with
-      Major, Semver (l1, _, _) -> Semver (inc_option l1, Some 0, Some 0)
-    | Minor, Semver (l1, l2, _) -> Semver (l1, inc_option l2, Some 0)
-    | Patch, Semver (l1, l2, l3) -> Semver (l1, l2, inc_option l3)
+let print_version (Semver (v1, v2, v3)) = sprintf "%d.%d.%d" v1 v2 v3
 
-  let dec_option = function None -> None | Some n -> Some (n - 1)
+(* Should this just expect a sorted list instead of sorting it itself? *)
+let query_version query versions =
+  let last ls = nth ls (length ls - 1) in
+  let compareQuery q v = match q, v with
+      QueryPatch (maj, min, patch), v' -> Semver (maj, min, patch) == v'
+    | QueryMinor (maj, min), Semver (v1, v2, _) -> maj == v1 && min == v2
+    | QueryMajor maj, Semver (v1, _, _) -> maj == v1 in
+  last (sort compare_version (filter (compareQuery query) versions))
 
-  let decrement_version = fun p v -> match p, v with
-      Major, Semver (l1, _, _) -> Semver (dec_option l1, Some 0, Some 0)
-    | Minor, Semver (l1, l2, _) -> Semver (l1, dec_option l2, Some 0)
-    | Patch, Semver (l1, l2, l3) -> Semver (l1, l2, dec_option l3)
+let parse_query input =
+  try sscanf input "%d.%d.%d" (fun v1 v2 v3 -> QueryPatch (v1, v2, v3))
+  with End_of_file ->
+    try sscanf input "%d.%d" (fun v1 v2 -> QueryMinor (v1, v2))
+    with End_of_file -> sscanf input "%d" (fun v1 -> QueryMajor v1)
 
-  let parse_version = fun input ->
-    try
-      sscanf input "%d.%d.%d" (fun v1 v2 v3 -> Semver (Some v1, Some v2, Some v3))
-    with End_of_file -> try
-      sscanf input "%d.%d" (fun v1 v2 -> Semver (Some v1, Some v2, None))
-    with End_of_file -> try
-      sscanf input "%d" (fun v1 -> Semver (Some v1, None, None))
-    with End_of_file -> Semver (None, None, None)
-
-  let print_version = fun (Semver (v1, v2, v3)) -> match v1, v2, v3 with
-      None, None, None -> ""
-    | Some v1, None, None -> sprintf "%d" v1
-    | Some v1, Some v2, None -> sprintf "%d.%d" v1 v2
-    | Some v1, Some v2, Some v3 -> sprintf "%d.%d.%d" v1 v2 v3
-    | _ -> raise Invalid_version
-end
+let print_query = function
+    QueryPatch (maj, min, patch) -> sprintf "%d.%d.%d" maj min patch
+  | QueryMinor (maj, min) -> sprintf "%d.%d" maj min
+  | QueryMajor maj -> sprintf "%d" maj
+   
+let query q vs = query_version (parse_query q) vs
