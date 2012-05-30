4966ac6b7a4a20c64fced518c56041746b0547cb
Tikhon Jelvis <tikhon@berkeley.edu>
2012-05-30 15:54:30 -0700

    Fixed up the test code a little bit.

--- lib_test/test_semver.ml
+++ lib_test/test_semver.ml
@@ -1,7 +1,7 @@
 open OUnit
 open Semver
 
-let test_parse_print = fun ver str -> begin
+let test_parse_print = fun ver str () -> begin
   assert_equal ver (parse_version str);
   assert_equal (print_version ver) str
 end
@@ -9,7 +9,7 @@ end
 let eq = fun v1 v2 -> assert_equal (compare_version v1 v2)  0
 let gt = fun v1 v2 -> begin
   assert_equal (compare_version v1 v2)  1;
-  assert_equal (compare_version v2 v1) -1;
+  assert_equal (compare_version v2 v1) (-1);
 end 
 
 let v = parse_version
@@ -18,21 +18,21 @@ let suite = "Semver suite">::: [
   "3 parts">:: test_parse_print (Semver (1, 1, 1)) "1.1.1";
   "long parts">:: test_parse_print (Semver (1243, 32415, 54535)) "1243.32415.54535";
 
-  "cmp">:: begin
+  "inc">:: begin fun () -> 
+    eq (v"0.0.1") (increment_version `Patch (v"0.0.0"));
+    eq (v"0.1.0") (increment_version `Minor (v"0.0.0"));
+    eq (v"1.0.0") (increment_version `Major (v"0.0.0"));
+    
+    eq (v"0.1.3") (increment_version `Patch (v"0.1.2"));
+    eq (v"1.1.0") (increment_version `Minor (v"1.0.2"));
+    eq (v"1.0.0") (increment_version `Major (v"0.1.2"));
+  end;
+
+  "cmp">:: begin fun () -> 
     eq (v"1.1.1") (v"1.1.1");
 
     gt (v"1.1.1") (v"1.1.0");
     gt (v"1.1.1") (v"1.0.1");
     gt (v"1.1.1") (v"0.1.1");
   end;
-
-  "inc">:: begin
-    eq (v"0.0.1") (increment_version Patch (v"0.0.0"));
-    eq (v"0.1.0") (increment_version Minor (v"0.0.0"));
-    eq (v"1.0.0") (increment_version Major (v"0.0.0"));
-    
-    eq (v"0.1.3") (increment_version Patch (v"0.1.2"));
-    eq (v"1.1.0") (increment_version Minor (v"1.0.2"));
-    eq (v"1.0.0") (increment_version Major (v"0.1.2"));
-  end
 ]
